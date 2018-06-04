91335f1a45f5fd8285b52a331ce32d2503cf4909
xt8541 <xt8541@XT8541T13AW10.grupoib.local>
2018-06-04 15:02:32 -0500

    Second Commit

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -2,7 +2,9 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world"); 
+		int a = 2;
+		int b = 4;
+		System.out.println("Suma: " + (2+4)); 
 
 	}
 
